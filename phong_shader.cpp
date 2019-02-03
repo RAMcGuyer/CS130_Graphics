7091c5720764f0f60c947c7492cf6870191612d7
Ross McGuyer <RAMcGuyer@gmail.com>
2019-02-03 00:21:07 -0800

    specular and shadows are working!

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -13,12 +13,30 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     vec3 color;
     vec3 l;
     vec3 r;
+    vec3 shadow;
     color = world.ambient_color*world.ambient_intensity*color_ambient;
-    for (Light* light : world.lights){
-        l = light->position - intersection_point; //Create a vector from intersection to light source
-        color += color_diffuse*light->Emitted_Light(l)*(std::max(dot(normal,l.normalized()), 0.0));
-        r = -l + 2*(dot(l, normal))*normal;
-        color += color_specular*light->Emitted_Light(l)*std::pow(std::max(dot(ray.direction, r.normalized()), 0.0), specular_power); 
+    if(world.enable_shadows){
+        for (Light* light : world.lights){
+            l = light->position - intersection_point; //vector from object to the light source
+            Ray shadowRay(intersection_point, l); //shadow ray for decting objects between light source and the intersetion point
+            Hit shadowHit = world.Closest_Intersection(shadowRay);
+            if(shadowHit.dist > 0){
+                shadow = shadowRay.Point(shadowHit.dist) - intersection_point;
+            }
+            if(!shadowHit.object || shadow.magnitude() > l.magnitude()){
+                color += color_diffuse*light->Emitted_Light(l)*(std::max(dot(normal,l.normalized()), 0.0));
+                r = -l + 2*(dot(l, normal))*normal;
+                color += color_specular*light->Emitted_Light(l)*std::pow(std::max(dot(-ray.direction, r.normalized()), 0.0), specular_power);
+            } 
+        }
+    }
+    else{
+        for(Light* light : world.lights){
+            l = light->position - intersection_point;
+            color += color_diffuse*light->Emitted_Light(l)*(std::max(dot(normal,l.normalized()), 0.0));
+            r = -l + 2*(dot(l, normal))*normal;
+            color += color_specular*light->Emitted_Light(l)*std::pow(std::max(dot(-ray.direction, r.normalized()), 0.0), specular_power);
+        }
     }
     return color;
 }
